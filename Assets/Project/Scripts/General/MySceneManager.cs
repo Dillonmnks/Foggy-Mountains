0854d18c244d34709ac2a258c6207d8263a83693
Dillonmnks <dillon.monks2104@gmail.com>
2026-09-05 13:47:13 +0200

    setting up main menu

--- Assets/Project/Scripts/General/MySceneManager.cs
+++ Assets/Project/Scripts/General/MySceneManager.cs
@@ -1,16 +1,52 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MySceneManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private static MySceneManager instance;
+
+    [Header("Name of the loading scene")]
+    public string loadingSceneName = "LoadingScreenName";
+
+    private string targetScene;
+
+    private void Awake()
     {
-        
+        if(instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void LoadScene(string sceneName)
     {
-        
+        instance.targetScene = sceneName;
+        SceneManager.LoadScene(instance.loadingSceneName);
+        instance.StartCoroutine(instance.LoadAsync());
+    }
+
+    private IEnumerator LoadAsync()
+    {
+        yield return null;
+
+        AsyncOperation op = SceneManager.LoadSceneAsync(targetScene);
+
+        op.allowSceneActivation = false;
+
+        while(!op.isDone)
+        {
+            Debug.Log($"Loading Scene: {op.progress * 100f}%");
+            if(op.progress >= 0.9f)
+            {
+                op.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
     }
 }
