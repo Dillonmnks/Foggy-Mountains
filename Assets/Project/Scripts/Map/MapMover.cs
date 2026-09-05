d3bf921d37a717846f8055822bc9ca656cb43a1d
DZO15 <kevinmilic187@gmail.com>
2026-09-05 21:55:30 +0200

    Map Mover dune

--- Assets/Project/Scripts/Map/MapMover.cs
+++ Assets/Project/Scripts/Map/MapMover.cs
@@ -1,16 +1,55 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MapMover : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject mapPrefab;
+    [SerializeField] private Transform startPos;
+    [SerializeField] private Transform endPos;
+    [SerializeField] private float speed;
+    [SerializeField] private float mapLength;
+
+    private List<GameObject> activeMaps = new();
+
+    private void Start()
     {
-        
+        activeMaps.Add(Instantiate(mapPrefab, startPos.position, Quaternion.identity));
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
     {
-        
+        MoveMap(); // noice
+        CheckMap(); // noice
+        DeleteMap(); // noice
     }
+
+    private void MoveMap()
+    {
+        Vector3 dir = (startPos.position - endPos.position).normalized;
+
+        foreach (var map in activeMaps) 
+            map.transform.position += dir * speed * Time.fixedDeltaTime;
+    }
+
+    private void CheckMap()
+    {
+        GameObject last = activeMaps[activeMaps.Count - 1];
+        if (Vector3.Distance(startPos.position, last.transform.position) >= mapLength)
+            activeMaps.Add(Instantiate(mapPrefab, startPos.position, Quaternion.identity));
+    }
+
+    private void DeleteMap()
+    {
+        GameObject first = activeMaps[0];
+
+        if(Vector3.Distance(first.transform.position, endPos.position) < 0.5f)
+        {
+            activeMaps.RemoveAt(0);
+            Destroy(first);
+        }
+    }
+
 }
+
+
